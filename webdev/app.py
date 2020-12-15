5545418f0186ed695dffc9bf9f66295d07cb59a5
Disguyranger <Disguyranger@gmail.com>
2020-12-15 11:32:31 -0500

    all changes and updates

---
+++ webdev/app.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+    
+import pymysql
+from flask_cors import CORS, cross_origin
+from config import remote_db_endpoint, remote_db_port
+from config import remote_db_name, remote_db_user, remote_db_pwd
+pymysql.install_as_MySQLdb()
+from sqlalchemy import func, create_engine
+
+app = Flask(__name__)
+
+
+engine = create_engine(f"mysql://{remote_db_user}:{remote_db_pwd}@{remote_db_endpoint}:{remote_db_port}/{remote_db_name}")
+
+# create route that renders index.html template
+@app.route("/")
+def home():    
+    return render_template("index.html")
+
+@app.route("/info")
+def info():
+    conn = engine.connect()
+    
+    query = f'''
+        SELECT 
+            *
+        FROM
+            geojsonLoc
+        
+    ''' 
+
+    game_df = pd.read_sql(query, con=conn)
+    game_json = game_df.to_json(orient='records')
+    conn.close()
+
+    return game_json
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ webdev/config.py
@@ -0,0 +1,5 @@
+remote_db_endpoint = 'gaming-storage.cydjkxyol2vm.us-east-2.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_db_user = 'admin'
+remote_db_pwd = 'Disguy#012389'
+remote_db_name = 'GameDeveloperHost'
